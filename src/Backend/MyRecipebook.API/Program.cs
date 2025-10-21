1794f65d0dfafb03bdcf3418de387b0335fc58d1
Calixto <gabrielcalixtosoares@gmail.com>
2025-10-21 17:50:56 -0300

    Feat: Adicionado pacote e configurado swagger

--- src/Backend/MyRecipebook.API/Program.cs
+++ src/Backend/MyRecipebook.API/Program.cs
@@ -2,12 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 
